032d041083dd5e2efaaf730e59508b9bbda6ec54
Vexturion <horizontedesucesos@outlook.com>
2022-03-02 21:10:06 -0600

    Nombre del proyecto: project-jsf-v1.1 Descripcion: Se creo el login de la aplicacion y se creo el metodo redireccionar a la pagina principal

---
+++ src/main/java/com/bassntunes/projectjsf/controllers/LoginController.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package com.bassntunes.projectjsf.controllers;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+/**
+ * @author Jose Alejandro Cruz
+ * clase que permite controlar el funcionamiento con la pantalla de login.xhtml
+ */
+@ManagedBean
+public class LoginController {
+
+	/**
+	 * usuario que ingresa en el login
+	 */
+	private String usuario;
+	
+	/**
+	 * password que permite al usuario ingresar
+	 */
+	private String password;
+	
+	/**
+	 * metodo que permite ingresar a la pantalla principal del proyecto
+	 */
+	public void ingresar() {
+		System.out.println("Usuario" + usuario);
+		if (usuario.equals("pepe") && password.equals("123")) {
+			try {
+				this.redireccionar("Principal.xhtml");
+			} catch (IOException e) {
+				FacesContext.getCurrentInstance().addMessage("formLogin:txtUsuario", new FacesMessage(FacesMessage.SEVERITY_FATAL, "La pagina no existe", ""));
+				e.printStackTrace();
+			}
+		} else {
+			FacesContext.getCurrentInstance().addMessage("formLogin:txtUsuario", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuario incorrecto", ""));
+			FacesContext.getCurrentInstance().addMessage("formLogin:txtPassword", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Password incorrecto", ""));
+		}
+	}
+
+	private void redireccionar(String pagina) throws IOException {
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		ec.redirect(pagina);
+		
+	}
+	
+	
+	/**
+	 * @return the usuario
+	 */
+	public String getUsuario() {
+		return usuario;
+	}
+
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
